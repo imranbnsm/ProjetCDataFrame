16c38bc4802346e318c094e0f3c8ce9c11f487e2
Zestellar <gossantoine@gmail.com>
2024-05-15 19:22:14 +0200

    fix

--- PARTIE2/CdataFrame.h
+++ PARTIE2/CdataFrame.h
@@ -8,7 +8,6 @@ CDATAFRAME *create_cdataframe(ENUM_TYPE *cdftype, int size);
 void delete_cdataframe(CDATAFRAME *cdf);
 void delete_Column(CDATAFRAME *cdf, const char *col_name);
 int get_cdataframe_cols_size(CDATAFRAME *cdf);
-CDATAFRAME *creer_Cdata_vide();
 void remplissage_Cdata(CDATAFRAME *cdf);
 CDATAFRAME* remplissage_Cdata_dur();
 void affichage_Cdata(CDATAFRAME *cdf);
